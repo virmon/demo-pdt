859d64e0beb15ba182b62583d331ff0ec4d9da6a
Virmon Tumulak <virmontumulak@gmail.com>
2026-09-03 02:59:32 -0700

    added Unit initial combat system

--- Assets/Scripts/Player/Unit.cs
+++ Assets/Scripts/Player/Unit.cs
@@ -1,18 +1,66 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    public static List<Unit> AllUnits = new List<Unit>();
+
+    private int maxHp;
+
+    public int hp;
+    public float sizeX = 0f;
+    public float sizeY = 0f;
+    public float attackInterval = 0.5f;
+    public float attackRange = 2f;
+    public int attackPow = 1;
     public float moveSpeed = 0.5f;
+
+    public Transform hpGauge;
+
+    private void Onnable()
+    {
+        AllUnits.Add(this);
+    }
+
+    private void Start()
+    {
+        maxHp = hp;
+    }
 
     private void Update()
     {
         Move();
+        Dead();
     }
-    
 
     // Move
     private void Move()
     {
         transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
     }
+
+    // Acquire a target within the attack range
+    // Attack the acquired target
+
+    // Unit takes damage
+    public void TakeDMG(int num)
+    {
+        hp -= num;
+        hpGauge.transform.localScale = new Vector3((float)hp / maxHp, 1, 1);
+    }
+
+    // Unit dies
+    private void Dead()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDisable()
+    {
+        AllUnits.Remove(this);
+    }
+
 }
