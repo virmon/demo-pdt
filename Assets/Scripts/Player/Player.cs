3401affd4549d85affc7969e7e0434a87172d939
Virmon Tumulak <virmontumulak@gmail.com>
2026-09-03 05:12:45 -0700

    added Unit Spawner

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,12 +4,16 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public GameObject[] unit;
+    public GameObject[] spawnPoint;
+
     public float speed = 8f;
-    
+
     Vector3 move;
 
     private void Update()
     {
+        UnitButtonPushing();
         Move();
     }
 
@@ -27,5 +31,40 @@ public class Player : MonoBehaviour
         currentPos.x = Math.Clamp(currentPos.x, -10f, 10f);
         currentPos.y = Math.Clamp(currentPos.y, -4f, 4f);
         transform.position = currentPos;
+    }
+
+    // spawn unit on button input
+    [NonSerialized] public bool isUnitPushing = false;
+    private float unitPushingTime = 0f;
+    public void OnSpawn(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            isUnitPushing = true;
+        }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            isUnitPushing = false;
+        }
+    }
+
+    private void UnitButtonPushing()
+    {
+        if (isUnitPushing)
+        {
+            unitPushingTime += Time.deltaTime;
+            if (unitPushingTime > 1f)
+            {
+                unitPushingTime = 0;
+                for (int i = 0; i < spawnPoint.Length; i++)
+                {
+                    float dy = Mathf.Abs(transform.position.y - spawnPoint[i].transform.position.y);
+                    if (dy < 0.8f)
+                    {
+                        Instantiate(unit[0], spawnPoint[i].transform.position, Quaternion.identity);
+                    }
+                }
+            }
+        }
     }
 }
