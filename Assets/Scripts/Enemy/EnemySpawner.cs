e01151c5ee1c8f53a7e467d8551c0f527616f351
Virmon Tumulak <virmontumulak@gmail.com>
2026-09-03 05:35:42 -0700

    added Enemy Spawner

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float spawnInterval = 12f;
+    public GameObject[] enemy;
+
+    private void Update()
+    {
+        SpawnLoop();
+    }
+
+    private float loopTime = 0f;
+    private float randomTime = 0f;
+
+    private void SpawnLoop()
+    {
+        loopTime += Time.deltaTime;
+        if (loopTime > spawnInterval + randomTime)
+        {
+            loopTime = 0f;
+            randomTime = Random.Range(0, 3f);
+
+            GameObject e = enemy[Random.Range(0, enemy.Length)];
+            Instantiate(e, transform.position, Quaternion.identity);
+        }
+    }
+}
