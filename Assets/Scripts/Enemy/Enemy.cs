bd4628ba1ed2f107f027f635b8793d2a4abe7da8
Virmon Tumulak <virmontumulak@gmail.com>
2026-09-03 04:01:49 -0700

    added Enemy initial combat system

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,18 +1,63 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    public static List<Enemy> AllEnemies = new List<Enemy>();
+
+    private int maxHp;
+
+    public int hp;
+    public float sizeX = 0f;
+    public float sizeY = 0f;
+    public float attackInterval = 0.5f;
+    public float attackRange = 2f;
+    public int attackPow = 1;
     public float moveSpeed = 0.5f;
+    public Transform hpGauge;
+
+    private void OnEnable()
+    {
+        AllEnemies.Add(this);
+    }
+
+    private void Start()
+    {
+        maxHp = hp;
+    }
 
     private void Update()
     {
         Move();
+        Dead();
     }
-    
+
 
     // Move
     private void Move()
     {
         transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+    }
+
+    // Unit takes damage
+    public void TakeDMG(int num)
+    {
+        hp -= num;
+        hpGauge.transform.localScale = new Vector3((float)hp / maxHp, 1, 1);
+    }
+
+    // Unit dies
+    private void Dead()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDisable()
+    {
+        AllEnemies.Remove(this);
     }
 }
